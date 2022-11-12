164c406dfa457ae055f041156c23dbc06ce7a0a5
Juanherrera117 <relujuca@gmail.com>
2022-11-12 11:44:35 -0500

    Casofecha

--- ejecutable/Test.java
+++ ejecutable/Test.java
@@ -6,15 +6,33 @@ public class Test
 {
     public static void main(String[] args) 
     {
-       Fecha f = new Fecha();
+       Fecha f = new Fecha(2, 10, 1970);
        f.setDia(2);
        f.setMes(10);
        f.setAnio(1970);
+
+       Fecha f2 = new Fecha();
 
        System.out.println("Dia: " + f.getDia());
        System.out.println("Mes: " + f.getMes());
        System.out.println("Año: " + f.getAnio());
        System.out.println(f.toString());
+
+       Fecha f3 = new Fecha("10/11/2022");
        
+       Fecha mif = new Fecha(2, 10, 1970);
+       mif.setDia(8);
+       mif.setMes(6);
+       mif.setAnio(1990);
+       System.out.println(mif.toString());
+
+       if(f.equals(mif))
+       {
+        System.out.println("Las fechas si son iguales");
+       }
+       else
+       {
+        System.out.println("Las fechas no son iguales");
+       }
     }
 }
--- modelo/Fecha.java
+++ modelo/Fecha.java
@@ -13,6 +13,43 @@ public class Fecha
     // Metodos
     //------------------
 
+    public Fecha(int d, int m, int a) 
+    {
+        this.dia = d;
+        this.mes = m;
+        this.anio = a;
+    }
+
+    //Constructor sin parametros
+    public Fecha()
+    {
+       this.dia = 0;
+       this.mes = 0;
+       this.anio = 0;
+    }
+
+    //Constructor con parametro String dd/mm/aaaa
+    public Fecha(String s)
+    {
+        //Posicion inicial de '/'
+        int pos1 = s.indexOf("/");
+
+        //Se busca la ultima ocurrencia de '/'
+        int pos2 = s.lastIndexOf("/");
+        
+        //Extraer el dia 
+        String sDia = s.substring(0, pos1);
+        dia = Integer.parseInt(sDia);
+
+        //Extraer el mes
+        String sMes = s.substring(pos1 + 1, pos2);
+        mes = Integer.parseInt(sMes);
+
+        //Extraer el año
+        String sAnio = s.substring(pos2 + 1);
+        anio = Integer.parseInt(sAnio);
+    }
+
     //Metodos de acceso (setter y getter)
     public int getDia()
     {
@@ -54,5 +91,25 @@ public class Fecha
     {
         Fecha otra = (Fecha)o;
         return (dia==otra.dia) && (mes==otra.mes) && (anio==otra.anio);
-    } 
+    }
+
+    public void addDias(int d)
+    {
+       int sum = fechaToDias() + d;
+       diasToFecha(sum);
+    }
+
+    private int fechaToDias()
+    {
+        return anio*360 + mes*30 + dia;
+    }
+
+    private void diasToFecha(int i)
+    {
+        anio = (int)(i/360);
+        mes = (int)(i/30);
+        dia = (int)(i);
+    }
+    
+    
 }
